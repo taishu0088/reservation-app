961512697e4dbdc439e37e5bcf091aae82b990f4
taishu <taishu0088@gmail.com>
2025-12-15 20:58:43 +0900

    名前平仮名に変更

--- app/components/ReservationSystem.tsx
+++ app/components/ReservationSystem.tsx
@@ -64,9 +64,9 @@ export default function ReservationSystem() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const users: Record<string, string> = {
-    山田: "keita225",
-    長谷川: "h0817",
-    川崎: "toya03",
+    けいた: "keita225",
+    たいしゅう: "h0817",
+    とうや: "toya03",
   };
 
   const handleLogin = () => {
